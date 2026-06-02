fb29ad31ebf067fa65747e60af01540d1a46dcf2
Arthur <160981535+Kwan-Daegun@users.noreply.github.com>
2026-06-02 12:56:37 +0800

    added tutorial

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -1,21 +1,43 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject loadingScreen;
+
     public void PlayGame(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadScene(sceneName));
     }
 
     public void Restart()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().name));
     }
 
     public void BackToMenu(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadScene(sceneName));
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator LoadScene(string sceneName)
+    {
+        loadingScreen.SetActive(true);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        while (operation.progress < 0.9f)
+            yield return null;
+
+        yield return new WaitForSeconds(0.5f);
+        operation.allowSceneActivation = true;
     }
 }
